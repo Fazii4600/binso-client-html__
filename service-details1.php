9c50f4a502352c9f2a67b3433989fe299e6314c2
Fazii4600 <146227872+Fazii4600@users.noreply.github.com>
2024-06-23 10:17:32 +0500

    comit 23 june

--- service-details1.php
+++ service-details1.php
@@ -220,44 +220,8 @@
 						<div class="row">
 							<!--Start Services Details Sidebar-->
 							<div class="col-xl-4 col-lg-4">
-								<div class="service-sidebar">
-									<!--Start Services Details Sidebar Single-->
-									<div class="sidebar-widget service-sidebar-single">
-
-										<div class="sidebar-service-list">
-											<ul>
-												<li class="current"><a href="service-details1.php" class="current"><i class="fas fa-angle-right"></i><span>Software Development</span></a></li>
-												<li ><a href="service-details2.php"><i class="fas fa-angle-right"></i><span>Bussiness Development</span></a></li>
-												<li ><a href="service-details.php"><i class="fas fa-angle-right"></i><span>Accounts & Finance</span></a></li>
-												<li><a href="service-details3.php"><i class="fas fa-angle-right"></i><span>Graphic Designing & Video Editing</span></a></li>
-												<li><a href="service-details3.php"><i class="fas fa-angle-right"></i><span>Digital Marketing</span></a></li>
-												<li><a href="service-details3.php"><i class="fas fa-angle-right"></i><span>SEO</span></a></li>
-												
-											</ul>
-										</div>
-
-										<div class="service-details-help">
-											<div class="help-shape-1"></div>
-											<div class="help-shape-2"></div>
-											<h2 class="help-title">Contact with <br/> us for any <br/> advice</h2>
-											<div class="help-icon">
-												<span class=" lnr-icon-phone-handset"></span>
-											</div>
-											<div class="help-contact">
-												<p>Need help? Talk to an expert</p>
-												<a href="tel:12463330079">+92 51 8745519 </a>
-											</div>
-										</div>
-
-										<!--Start Services Details Sidebar Single-->
-										<div class="sidebar-widget service-sidebar-single mt-4">
-											<div class="service-sidebar-single-btn wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="1200m">
-												<a href="#" class="theme-btn btn-style-one d-grid"><span class="btn-title"><span class="fas fa-file-pdf"></span> download pdf file</span></a>
-											</div>
-										</div>
-									</div>
-									<!--End Services Details Sidebar-->
-								</div>
+								 
+								 <?php include 'services-sidebar.php'; ?>
 							</div>
 
 							<!--Start Services Details Content-->
---
+++ services-sidebar.php
@@ -0,0 +1,38 @@
+<div class="service-sidebar">
+									<!--Start Services Details Sidebar Single-->
+									<div class="sidebar-widget service-sidebar-single">
+
+										<div class="sidebar-service-list">
+											<ul>
+												<li><a href="service-details1.php" ><i class="fas fa-angle-right"></i><span>Software Development</span></a></li>
+												<li><a href="service-details2.php"><i class="fas fa-angle-right"></i><span>Bussiness Development</span></a></li>
+												<li><a href="service-details3.php"><i class="fas fa-angle-right"></i><span>Accounts & Finance</span></a></li>
+												<li><a href="service-details4.php"><i class="fas fa-angle-right"></i><span>Graphic Designing & Video Editing</span></a></li>
+												<li><a href="service-details5.php"><i class="fas fa-angle-right"></i><span>Digital Marketing</span></a></li>
+												<li class="current"><a href="service-details6.php"><i class="fas fa-angle-right"></i><span>SEO</span></a></li>
+												
+											</ul>
+										</div>
+
+										<div class="service-details-help">
+											<div class="help-shape-1"></div>
+											<div class="help-shape-2"></div>
+											<h2 class="help-title">Contact with <br/> us for any <br/> advice</h2>
+											<div class="help-icon">
+												<span class=" lnr-icon-phone-handset"></span>
+											</div>
+											<div class="help-contact">
+												<p>Need help? Talk to an expert</p>
+												<a href="tel:12463330079">+92 51 8745519 </a>
+											</div>
+										</div>
+
+										<!--Start Services Details Sidebar Single-->
+										<div class="sidebar-widget service-sidebar-single mt-4">
+											<div class="service-sidebar-single-btn wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="1200m">
+												<a href="#" class="theme-btn btn-style-one d-grid"><span class="btn-title"><span class="fas fa-file-pdf"></span> download pdf file</span></a>
+											</div>
+										</div>
+									</div>
+									<!--End Services Details Sidebar-->
+								</div>
